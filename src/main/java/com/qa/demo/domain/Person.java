05f78c0934adc3e7604a293e7f3c8974465f7f6e
Paul <pstyp94@gmail.com>
2022-04-12 11:01:01 +0100

    connected to h2 database

--- src/main/java/com/qa/demo/domain/Person.java
+++ src/main/java/com/qa/demo/domain/Person.java
@@ -1,11 +1,26 @@
 package com.qa.demo.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity //tells Spring it's a table
 public class Person {
 
 	//ATTRIBUTES
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //AUTO-INCREMENT
 	private Integer id;
+	
+	@Column(nullable=false)
 	private String name;
+	
+	@Column(nullable=false)
 	private Integer age;
+	
+	@Column(nullable=false)
 	private Integer height;
 
 	
